2a2d2ff1b12b42cf7ddd1da004419125e8c0ad63
Ahmed Ezzat <ah.ezzat.muh@gmail.com>
2022-10-24 07:44:43 +0200

    added saveFile functionality

--- texteditor.py
+++ texteditor.py
@@ -7,6 +7,16 @@ root = Tk("Text Editor")
 text = Text(root)
 text.grid()
 
+def saveas():
+	global text
+	t = text.get("1.0", "end-1c")
+	savelocation = tkinter.filedialog.asksaveasfilename()
+	file1 = open(savelocation, "w+")
+	file1.write(t)
+	file1.close()
+
+button = Button(root, text="Save", command=saveas)
+button.grid()
 
 root.mainloop()
 
