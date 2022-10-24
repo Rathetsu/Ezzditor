02921fc9e5a17826601d9cbacea18badc20f7086
Ahmed Ezzat <ah.ezzat.muh@gmail.com>
2022-10-24 07:53:09 +0200

    added simple font menu

--- texteditor.py
+++ texteditor.py
@@ -7,6 +7,8 @@ root = Tk("Text Editor")
 text = Text(root)
 text.grid()
 
+# Button to save the file
+
 def saveas():
 	global text
 	t = text.get("1.0", "end-1c")
@@ -17,6 +19,38 @@ def saveas():
 
 button = Button(root, text="Save", command=saveas)
 button.grid()
+
+##########################################
+
+
+# Font menu button
+
+def fontHelvetica():
+	global text
+	text.config(font="Helvetica")
+
+def fontCourier():
+	global text
+	text.config(font="Courier")
+
+def fontTimes():
+	global text
+	text.config(font="Times")
+
+font = Menubutton(root, text="Font")
+font.grid()
+font.menu = Menu(font, tearoff=0)
+font["menu"] = font.menu
+
+helvetica = IntVar()
+courier = IntVar()
+times = IntVar()
+
+font.menu.add_radiobutton(label="Helvetica", variable=helvetica, command=fontHelvetica)
+font.menu.add_radiobutton(label="Courier", variable=courier, command=fontCourier)
+font.menu.add_radiobutton(label="Times", variable=times, command=fontTimes)
+
+##########################################
 
 root.mainloop()
 
